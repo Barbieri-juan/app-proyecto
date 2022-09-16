9ef07e267afefe9d2c40b1747d3bf8b43c9ee694
unknown <barbieri-juan@hotmail.com>
2022-09-15 23:05:49 -0300

    deploy

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+Buenos días. Gracias por visitar a "Las Pastas de la Nonna".
+
+Pagina web diseñada para restautante de comida Italiana de la más alta calidad.
+Encontran que esta pág. ha sido desarrolada con motor de plantillas HBS. Conteniendo sus respectivas funcionalidades JS y el estilo con  CSS.
+A su vez está conectada con database en Mysql y deployada con Herokou.
+
+MI primer pág. web desarrolada por completo. Gracias por su atenci{on.
+
+saludos¡¡¡¡
---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+require('dotenv').config();
+const path = require('path'); 
+const hbs = require('hbs'); 
+const mysql = require('mysql2'); 
+const nodemailer = require('nodemailer'); 
+const { get } = require('http');
+
+const app = express(); 
+const PORT = process.env.PORT || 8080; 
+
+const conexion = mysql.createConnection ({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+
+//Conexión a DataBase
+/*conexion.connect((err) => {
+    if(err){
+    console.error(`Error en la conexion: ${err.stack}`);
+    return;
+}
+    console.log(`Conectado a base de datos ${process.env.DATABASE}`);
+});*/
+
+//Middelwares
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')))
+
+//configuración del motor de plantillas
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+// conexion con partes de la pagina
+app.get('/', (req, res, next)=>{
+    res.render('index', {
+        style:'index.css'   
+    })
+})
+app.get('/Nosotros', (req, res) =>{
+    res.render('Nosotros', {
+        titulo: 'Sobre Nosotros',
+        style:'index.css'
+    })
+});
+app.get('/Chef', (req, res) =>{
+    res.render('Chef', {
+        titulo: 'Chef',
+        style:'index.css'
+    })
+});
+
+app.get('/Contacto', (req, res) =>{
+    res.render('Contacto', {
+        titulo: 'Contacto',
+        style:'index.css'
+    })
+});
+app.get('/Menu', (req, res) =>{
+    res.render('Menu', {
+        titulo: 'Menu',
+        style:'index.css'
+    })
+});
+
+app.post('/Contacto', (req, res) =>{
+    const {Nombre, Apellido, Correo, Telefono, Mensaje}= req.body;
+    console.log(Nombre, Apellido, Correo, Telefono, Mensaje);
+
+    let datos = {
+        Nombre: Nombre,
+        Apellido: Apellido,
+        Correo: Correo,
+        Telefono: Telefono,
+        Mensaje: Mensaje,
+    };
+    let sql = 'INSERT INTO Contacto SET ?';
+    conexion.query(sql, datos, (error, result)=>{
+    
+    res.render('Index',{
+        titulo: 'Contacto',
+        style:'index.css'
+    });
+    });       
+});
+
+
+app.listen(PORT, () => {
+    console.log(`El servidor está trabajando en el Puerto ${PORT}`);
+});
+
+
+
+
+
+
